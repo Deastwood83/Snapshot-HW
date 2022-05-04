889e0552e36773701e1eaa7c64e5797b522ea4fe
unknown <danieleastwood4@gmail.com>
2022-05-03 21:54:19 -0400

    snapshot

---
+++ src/components/GitHubCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+
+    const GitHubCard = () => {
+        <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="https://images.mein-mmo.de/medien/2022/01/elden-ring-character-editor-titelbild.jpg" />
+  <Card.Body>
+    <Card.Title>Deastwood83</Card.Title>
+    <Card.Text>
+      I like to go to the gym, and I like boxing.
+    </Card.Text>
+    <Button variant="primary">Learn More</Button>
+  </Card.Body>
+</Card>
+}
+
+export default GitHubCard
